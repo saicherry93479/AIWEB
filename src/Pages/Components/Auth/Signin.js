ee38f9d119a7a178c4ab51b370b2337d5ba492a8
saicherry93479 <saicherry93479@gmail.com>
2023-03-02 00:42:54 +0530

    auth pages updated three

--- src/Pages/Components/Auth/Signin.js
+++ src/Pages/Components/Auth/Signin.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import SignupImage from '../../../Assets/Images/SignupImage'
 import  '../../Styles/Auth/SignIn.css'
 import PredictCustomInput from '../Prediction/PredictCustomInput'
